7470c9d22c0d32c02bb61cd200ff250d83273693
Karen Payne <Karen.1.payne@oregon.gov>
2020-11-19 09:53:05 -0800

    Roughed out generic interface

---
+++ EntityFramework5North/Interfaces/IModelEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthEntityLibrary.Interfaces
+{
+    public interface IModelEntity<T>
+    {
+        T GetById(int id);
+        T Update(T model);
+        T Add(T model);
+        T Delete(int id);
+        int Commit();
+        Task<int> CommitAsync(); 
+        List<T> GetAll();
+    }
+}
