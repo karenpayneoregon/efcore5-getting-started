3aa3cc72c010cadfcf053fbfd717dc90bf83c6a4
Karen Payne <Karen.1.payne@oregon.gov>
2020-12-12 18:26:48 -0800

    minor updates

---
+++ StoredProcedureInsertNewCategory/ProcStrips/script.sql
@@ -0,0 +1,25 @@
+﻿USE [NorthWind2020]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[uspInsertCategory]  
+    @CategoryName NVARCHAR(15), 
+    @Description ntext, 
+    @Identity INT OUT 
+AS 
+BEGIN 
+    -- SET NOCOUNT ON added to prevent extra result sets from 
+    -- interfering with SELECT statements. 
+    SET NOCOUNT ON; 
+INSERT INTO dbo.Categories (CategoryName, Description) VALUES (@CategoryName, @Description);
+SET @Identity = SCOPE_IDENTITY() 
+ 
+END 
+ 
+GO
